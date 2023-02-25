8bb69e21a48ddb229961dfb69680fb56b1fde9c0
Kamil Nafalski <kamilnafalski@gmail.com>
2023-02-25 18:15:42 +0100

    Add initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,7 @@
       thisProduct.renderInMenu();
       console.log('new Product;', thisProduct);
       
+      thisProduct.initAccordion();
     }
     renderInMenu(){
       const thisProduct = this;
@@ -75,7 +76,33 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
-  }  
+
+    initAccordion(){
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('clickableTrigger:', clickableTrigger);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        console.log(activeProduct);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct == thisProduct.element){
+          activeProduct.classlist.remove('active');
+        }
+        //thisProduct.element.remove('active')};
+        
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
+
+    }
+
+  }
+    
 
   const app = {
     initMenu: function(){
