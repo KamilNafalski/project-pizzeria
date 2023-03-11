502c70409f8c2e7c4965e9d13a697280fc67e91b
Kamil Nafalski <kamilnafalski@gmail.com>
2023-03-11 07:50:19 +0100

    Add product to cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -172,6 +172,7 @@
       thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
     }
     
@@ -227,6 +228,8 @@
       }
       /* multiply price by amount */
       price *= thisProduct.amountWidget.value;
+
+      thisProduct.priceSingle = price;
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
@@ -238,6 +241,58 @@
         thisProduct.processOrder();
       });
     }
+
+    addToCart(){
+      const thisProduct = this;
+
+      app.cart.add(thisProduct.prepareCartProduct());
+    }
+
+    prepareCartProduct(){
+      const thisProduct = this;
+
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+        params: thisProduct.prepareCartProductParams()
+      };
+      return productSummary;
+    }
+
+    prepareCartProductParams(){
+      const thisProduct = this;
+          
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      
+      const params = {};
+      
+      for(let paramId in thisProduct.data.params) {
+        
+        const param = thisProduct.data.params[paramId];
+        params[paramId] = {
+          label: param.label,
+          options: {}
+        };
+        
+        for(let optionId in param.options) {
+          
+          const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(formData[paramId] && formData[paramId].includes(optionId)) {         
+           
+            if(optionSelected) { 
+              // option is selected!
+              params[paramId].options[optionId] = option.label;
+            }
+          } 
+        }
+      }
+      return params;
+    }
+    
   }      
 
   class AmountWidget{
@@ -319,6 +374,8 @@
       thisCart.dom.wrapper = element;
 
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
     }
 
     initActions(){
@@ -326,6 +383,14 @@
       thisCart.dom.toggleTrigger.addEventListener('click', function(){
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+    }
+
+    add(menuProduct){
+      const thisCart = this;
+      const generatedHTML = templates.cartProduct(menuProduct);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      thisCart.dom.productList.appendChild(generatedDOM);
+      console.log('adding product', menuProduct);
     }
   }
 
